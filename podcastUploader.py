010698a47c9bfd2cbf220dfef10cfe6854ee7ab5
Ryan Measel <ryanmeasel@gmail.com>
2016-07-28 13:49:38 -0700

    Added some whitespace for much needed clarity

--- podcastUploader.py
+++ podcastUploader.py
@@ -2,12 +2,12 @@
 """Convert, tag, and upload podcast episodes."""
 # to do
 # - configuration files
+# - dynamically generate the feed header everytime
 
 import argparse
 import datetime
 import mutagen.id3
 import os
-import sys
 import re
 from mutagen.easyid3 import EasyID3
 from mutagen.id3 import ID3, APIC
@@ -31,6 +31,11 @@ def throwFatalError(errorMessage):
     """Convenience function to throw error messages and exit the program."""
     print ansiColor.RED + "**ERROR**: " + errorMessage + ansiColor.NOCOLOR
     exit()
+
+
+def throwWarning(warnMessage):
+    """Convenience function to print warning messages."""
+    print ansiColor.YELLOW + "**WARNING**: " + warnMessage + ansiColor.NOCOLOR
 
 
 def log(logMessage):
@@ -71,6 +76,7 @@ def extensionValid(filename, ext):
 
 def parseArgs():
     """Parse the command line arguments."""
+    # Define the parser
     parser = argparse.ArgumentParser(description='Transcode, tag, and upload \
                                      podcast episodes.')
     parser.add_argument('-c', '--config', required=True,
@@ -81,6 +87,7 @@ def parseArgs():
                         help='Description of the episode.')
     parser.add_argument('audioFile', help='WAV or MP3 file to be added to the \
                         feed. WAV files will be transcoded to 128 Kbps MP3s.')
+
     # Returns a namespace containing the parsed arguments
     return parser.parse_args()
 
@@ -91,22 +98,28 @@ def processTitleAndDesc(title, desc):
     if not title:
         title = unicode(raw_input(ansiColor.BLUE + "Enter a title: " +
                                   ansiColor.NOCOLOR))
-    if not title:
-        throwFatalError("A title must be supplied.")
+
+        # Check that something was entered, enforcing a non-empty title
+        if not title:
+            throwFatalError("A title must be supplied.")
+
     if containsInvalidXmlTextChars(title):
         throwFatalError("\'Title\' may only contain valid XML" +
                         "characters (e.g., not '<' and '&').")
     log("Title is valid.")
+
     # Get the description if it wasn't passed in on the command line.
     if not desc:
         desc = unicode(raw_input(ansiColor.BLUE + "Enter a description: " +
                                  ansiColor.NOCOLOR))
-    if not desc:
-        throwFatalError("A description must be supplied.")
+        if not desc:
+            throwFatalError("A description must be supplied.")
+
     if containsInvalidXmlTextChars(desc):
         throwFatalError("\'Description\' may only contain valid XML" +
                         "characters (e.g., not '<' and '&').")
     log("Description is valid.")
+
     return title, desc
 
 
@@ -115,10 +128,12 @@ def processAudio(filename):
     # Check that the file exists
     if not fileExists(filename):
         throwFatalError("\'" + filename + "\' does not exist.")
+
     # Check that the file is either a WAV or an MP3
     if not extensionValid(filename, '.WAV') and \
        not extensionValid(filename, '.MP3'):
         throwFatalError("The audio file must be a WAV or MP3.")
+
     # If it's a WAV, then transcode it to MP3
     if extensionValid(filename, '.WAV'):
         return transcodeAudio(filename)
@@ -131,8 +146,9 @@ def transcodeAudio(filename):
     log("Transcoding to MP3...")
     # Check if the mp3 already exists
     fileroot, _ = os.path.splitext(filename)
-    if os.path.isfile(fileroot + ".mp3"):
+    if fileExists(fileroot + ".mp3"):
         throwFatalError("\'" + fileroot + ".mp3\' already exists.")
+
     # Transcode the mp3
     try:
         os.system("lame -V2 -h  --quiet %s.wav %s.mp3" % (fileroot, fileroot))
@@ -141,9 +157,10 @@ def transcodeAudio(filename):
     return fileroot + ".mp3"
 
 
-def addTags(filename, title, desc):
+def addTags(config, filename, title, desc):
     """Add ID3 tags and cover image to the mp3."""
     log("Adding ID3 tags...")
+
     # Use EasyID3 to apply text tags since it's less complicated than ID3.
     # Sadly, EasyID3 can not handle album art so we'll do that separate.
     try:
@@ -151,29 +168,35 @@ def addTags(filename, title, desc):
     except mutagen.id3.ID3NoHeaderError:
         meta = MP3(filename, ID3=EasyID3)
         meta.add_tags()
+
     # Set tags
     meta["title"] = title
-    meta["artist"] = unicode("Sharptown Church")
+    meta["artist"] = unicode(config["artist"])
     meta["date"] = unicode(str(datetime.datetime.now().year))
-    meta["album"] = unicode("Sharptown Church Podcast")
+    meta["album"] = unicode(config["album"])
     meta.save()
+
     # Create the image tag
     log("Adding the Cover Image...")
-    imageFilename = "podcastLogo1400.jpg"
-    _, imageFileExtension = os.path.splitext(imageFilename)
+    imageFilepath = config["imageFilepath"]
+    _, imageFileExtension = os.path.splitext(imageFilepath)
     imageTag = APIC()
+
     # Determine the file type
     if imageFileExtension.lower() == ".png":
         imageTag.mime = 'image/png'
     elif imageFileExtension.lower() == ".jpg":
         imageTag.mime = 'image/jpeg'
     else:
-        throwFatalError("Invalid cover image. Must be a PNG or JPG.")
+        throwFatalError("Cover image must be a PNG or JPG.")
+
     # Set the image tags
     imageTag.encoding = 3  # 3 is for utf-8
     imageTag.type = 3      # 3 is for cover image
     imageTag.desc = u'Cover'
-    imageTag.data = open(imageFilename, 'rb').read()
+    with open(imageFilepath, 'rb') as f:
+        imageTag.data = f.read()
+
     # Add the tag using ID3
     try:
         audio = MP3(filename, ID3=ID3)
@@ -183,92 +206,130 @@ def addTags(filename, title, desc):
         raise
 
 
-def addEntryToXml(filename, title, desc):
+def addEntryToXml(config, filename, title, desc):
     """Add an entry into the XML for this episode."""
     log("Adding entry to the XML file...")
-    # Create the backup directory if it doesn't already exist
-    backupDir = "./xmlBackups"
+
+    # Get the date in UTC
+    utcnow = datetime.datetime.utcnow()
+
+    # Create the XML backup directory if it doesn't already exist
+    backupDir = config["xmlBackupDir"]
     try:
         os.makedirs(backupDir)
     except OSError:
         if not os.path.isdir(backupDir):
             raise
-    # Get the date in UTC
-    utcnow = datetime.datetime.utcnow()
+
     # Create the backup file by copying the XML file into the backup directory
-    xmlFilename = "sharptownPodcast.xml"
-    if not os.path.isfile(xmlFilename):
-        throwFatalError("\'" + xmlFilename + "\' is not a file.")
+    xmlFilepath = config["xmlFilepath"]
+    if not os.path.isfile(xmlFilepath):
+        throwFatalError("\'" + xmlFilepath + "\' is not a file.")
     try:
-        os.system("cp %s %s/%s_%s.xml" % (xmlFilename, backupDir,
-                  xmlFilename[:-4], utcnow.strftime("%Y%m%d-%H%M%S")))
+        os.system("cp %s %s/%s_%s.xml" % (xmlFilepath, backupDir,
+                  xmlFilepath[:-4], utcnow.strftime("%Y%m%d-%H%M%S")))
     except OSError:
         raise
-    # Open the XML file for reading
-    try:
-        f = open(xmlFilename, 'r')
-        # Read all the lines from the backup which will be written into the new
-        # file along with the new entry
+
+    # Read all the lines from the backup which will be written into the new
+    # file along with the new entry
+    with open(xmlFilepath, 'r') as f:
         lines = f.readlines()
-        f.close()
-    except IOError:
-        raise
-    # Open the XML file for writing
+
+    # Calculate the length of the episode in hours, minutes, and seconds
     try:
-        f = open(xmlFilename, 'w')
-        # Calculate the length of the episode in hours, minutes, and seconds
-        try:
-            audioFile = MP3(filename)
-            hours, rem = divmod(audioFile.info.length, 3600)
-            mins, secs = divmod(rem, 60)
-        except:
-            raise
+        audioFile = MP3(filename)
+        hours, rem = divmod(audioFile.info.length, 3600)
+        mins, secs = divmod(rem, 60)
+    except:
+        raise
+
+    # Open the XML file for writing
+    with open(xmlFilepath, 'w') as f:
         # Write the header
         for i in range(32):
             f.write(lines[i])
         f.write("<lastBuildDate>" + utcnow.strftime("%a, %d %b %Y %H:%M:%S") +
                 "</lastBuildDate>\n")
+
         # Write the new entry
         f.write("<item>\n")
         f.write(" <title>" + title + "</title>\n")
-        f.write(" <link>http://sharptown.org</link>\n")
-        f.write(" <itunes:author>Sharptown Church</itunes:author>\n")
-        f.write(" <dc:creator>Sharptown Church</dc:creator>\n")
+        f.write(" <link>" + config["link"] + "</link>\n")
+        f.write(" <itunes:author>" + config["itunesAuthor"] +
+                "</itunes:author>\n")
+        f.write(" <dc:creator>" + config["dcCreator"] + "</dc:creator>\n")
         f.write(" <description>" + desc + "</description>\n")
         f.write(" <content:encoded>" + desc + "</content:encoded>\n")
         f.write(" <pubDate>" + utcnow.strftime("%a, %d %b %Y %H:%M:%S") +
                 " UTC</pubDate>\n")
         f.write(" <itunes:summary>" + desc + "</itunes:summary>\n")
-        f.write(" <itunes:keywords>Sharptown, Methodist, Church, " +
-                "Christianity, Religion, Jesus, God</itunes:keywords>\n")
+        f.write(" <itunes:keywords>" + config["itunesKeywords"] +
+                "</itunes:keywords>\n")
         f.write(" <itunes:duration>%02d:%02d:%02d</itunes:duration>\n"
                 % (hours, mins, secs))
-        f.write(" <category>Religion &amp; Spirituality</category>\n")
-        f.write(" <enclosure url=\"http://sharptown.org/podcast/" +
-                sys.argv[1] + "\" length=\"" +
+        f.write(" <category>" + config["category"] + "</category>\n")
+
+        # The "webFolder" must end in a slash
+        webFolder = config["webFolder"]
+        if not webFolder.endswith('/'):
+            webFolder += "/"
+        f.write(" <enclosure url=\"" + webFolder +
+                filename + "\" length=\"" +
                 str(os.path.getsize(filename)) +
                 "\" type=\"audio/mpeg3\" />\n")
-        f.write(" <guid>http://sharptown.org/podcast/" + filename +
-                "</guid>\n")
-        f.write(" <itunes:explicit>no</itunes:explicit>\n")
+        f.write(" <guid>" + webFolder + filename + "</guid>\n")
+        f.write(" <itunes:explicit>" + config["itunesExplicit"] +
+                "</itunes:explicit>\n")
         f.write("</item>\n")
+
         # Write the remainder of the file
         for i in range(33, len(lines)):
             f.write(lines[i])
-        # Clean up!
-        f.close()
-    except IOError:
-        raise
+
+
+def parseConfigFile(configFile):
+    """Parse the configuration file."""
+    # Read in the config file
+    if not fileExists(configFile):
+        throwFatalError("\'" + configFile + "\' does not exist.")
+    with open(configFile) as f:
+        contents = f.readlines()
+
+    # Load the contents into a dictionary
+    config = dict()
+    for line in contents:
+        # Each line should have a single parameter delimited by "=".
+        # Skip if the line is only whitespace.
+        if line.isspace():
+            continue
+
+        # Skip if the line if it is a comment (starts with "#")
+        if line[0] == "#":
+            continue
+        fields = line.split('=', 1)
+        if len(fields) < 2:
+            throwWarning("\'" + fields[0] + "\' has not been specified.")
+
+        # Assign the key value pair and strip whitespace
+        key = fields[0].lstrip().rstrip()
+        value = fields[1].lstrip().rstrip()
+
+        # Strip doubles quotes from the value if it has it
+        if value.startswith('"') and value.endswith('"'):
+                value = value[1:-1]
+        config[key] = value
+    return config
 
 
 def main():
     """Main function."""
     args = parseArgs()
-    # config = parseConfigFile(args.config)
+    config = parseConfigFile(args.config)
     title, desc = processTitleAndDesc(args.title, args.description)
     mp3Filename = processAudio(args.audioFile)
-    addTags(mp3Filename, title, desc)
-    addEntryToXml(mp3Filename, title, desc)
+    addTags(config, mp3Filename, title, desc)
+    addEntryToXml(config, mp3Filename, title, desc)
     print ansiColor.GREEN + "++ Done. " + u'\u2714' + ansiColor.NOCOLOR
 
 
