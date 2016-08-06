d2d1cab9a37f6db8f90c0df883cd7e4fb186111f
Ryan Measel <ryanmeasel@gmail.com>
2016-08-06 02:42:08 -0700

    moved parseConfigFile() from xml.py to here

--- penpen/penpen.py
+++ penpen/penpen.py
@@ -1,32 +1,20 @@
 #!/usr/bin/env python
 """Convert, tag, and upload podcast episodes."""
 # to do
-# - configuration files
-# - cp the backup back to the original if the write fails
-# - dynamically generate the feed header everytime
+# - config file documentation
+# - copyrights
+
 
 import argparse
 import logging
+import os
 
 import audio
-import xml
+import rss
 
 # Configure logger globally
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
-
-
-class ansiColor:
-    """ANSI terminal colors."""
-
-    RED = '\033[1;31m'
-    GREEN = '\033[1;32m'
-    YELLOW = '\033[1;33m'
-    BLUE = '\033[1;34m'
-    PURPLE = '\033[1;35m'
-    NOCOLOR = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
 
 
 def parseArgs():
@@ -47,38 +35,75 @@ def parseArgs():
     return parser.parse_args()
 
 
-def validateTextField(field, fieldName):
+def validateTextField(fieldName, field):
     """Validate a text field. Prompt if empty."""
     # Get the title if it wasn't passed in on the command line.
     if not field:
-        field = unicode(raw_input(ansiColor.BLUE +
-                                  "Enter the " +
-                                  fieldName.lower() +
-                                  ": " +
-                                  ansiColor.NOCOLOR))
+        field = unicode(raw_input("Enter the episode " + fieldName.lower() +
+                                  ": "))
 
         # Check that something was entered, enforcing a non-empty title
         if not field:
             logging.fatal("\'" + fieldName + "\' must be supplied.")
             exit(1)
 
-    if xml.containsInvalidTextChars(field):
-        logger.fatal("\'" + fieldName + "\' may only contain valid XML" +
-                     "characters (e.g., not '<' and '&').")
+    return field
+
+
+def parseConfigFile(configFile):
+    """Parse the configuration file."""
+    # Read in the config file
+    if not os.path.isfile(configFile):
+        logger.fatal("\'" + configFile + "\' does not exist.")
         exit(1)
 
-    return field
+    with open(configFile) as f:
+        contents = f.readlines()
+
+    # Load the contents into a dictionary
+    config = dict()
+
+    for line in contents:
+        # Each line should have a single parameter delimited by "=".
+        # Skip if the line is only whitespace.
+        if line.isspace():
+            continue
+
+        # Skip if the line if it is a comment (starts with "#")
+        if line[0] == "#":
+            continue
+
+        # Split on the "=" delimiter
+        fields = line.split('=', 1)
+        if len(fields) < 2:
+            logger.warning("\'" + fields[0] + "\' has not been specified.")
+
+        # Assign the key value pair and strip whitespace
+        key = fields[0].lstrip().rstrip()
+        value = fields[1].lstrip().rstrip()
+
+        # Strip doubles quotes from the value if it has it
+        if value.startswith('"') and value.endswith('"'):
+            value = value[1:-1]
+
+        config[key] = value
+
+    return config
 
 
 def main():
     """Main function."""
+    # Parse arguments and load parameters
     args = parseArgs()
-    config = xml.parseConfigFile(args.config)
-    title = validateTextField(args.title, "Title")
-    desc = validateTextField(args.description, "Description")
-    filename, length = audio.process(args.audioFile, config, title, desc)
-    xml.addItem(filename, config, title, desc, length)
-    logger.info(ansiColor.GREEN + "++ Done. " + u'\u2714' + ansiColor.NOCOLOR)
+    config = parseConfigFile(args.config)
+    title = validateTextField('Title', args.title)
+    desc = validateTextField('Description', args.description)
+
+    # Transcode and tag the audio
+    mp3File, duration = audio.process(args.audioFile, config, title, desc)
+
+    # Add to the RSS feed
+    rss.addEpisode(config, title, desc, mp3File, duration)
 
 
 if __name__ == "__main__":
