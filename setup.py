f711dc0586a26fd77a1037f4ae245a9db652b9e6
Ryan Measel <ryanmeasel@gmail.com>
2017-02-19 21:13:23 -0800

    Added classifiers to the package dist

--- setup.py
+++ setup.py
@@ -49,5 +49,26 @@ setup(
         'console_scripts': [
             'penpen=penpen:main',
         ]
-    }
+    },
+
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 4 - Beta',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Information Technology',
+        'Topic :: Utilities',
+
+        # Pick your license as you wish (should match "license" above)
+         'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7'
+    ]
 )
